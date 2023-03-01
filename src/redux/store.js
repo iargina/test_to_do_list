8823a350b3e52bd44aa246541e89a052fa6abceb
Irina <iargina.irina@gmail.com>
2023-03-01 02:21:52 +0200

    slice update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { toDoReducer } from './todo/todoSlice';
-import { modalReducer } from './modal/modal-slice';
 
 export const store = configureStore({
   reducer: {
     toDo: toDoReducer,
-    modal: modalReducer,
   },
 });
